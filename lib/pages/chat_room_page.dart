451501bdd119cd5657cf5a138173343da8d82378
ynsz <99311398+ynsz@users.noreply.github.com>
2026-02-05 22:46:37 +0900

    トークルームにメッセージを表示可能に

--- lib/pages/chat_room_page.dart
+++ lib/pages/chat_room_page.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_chat_app/modules/message.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart' hide TextDirection;
 
 class ChatRoomPage extends StatelessWidget {
   const ChatRoomPage({required this.userName, super.key});
@@ -7,6 +9,76 @@ class ChatRoomPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text(userName)));
+    final messageList = [
+      Message(
+        id: '1',
+        text: 'こんにちは',
+        senderId: 'hoge',
+        createdAt: DateTime.now(),
+        updatedAt: DateTime.now(),
+      ),
+      Message(
+        id: '2',
+        text: 'どうもー！',
+        senderId: 'fuga',
+        createdAt: DateTime.now(),
+        updatedAt: DateTime.now(),
+      ),
+      Message(
+        id: '3',
+        text: '元気にしてますか？',
+        senderId: 'hoge',
+        createdAt: DateTime.now(),
+        updatedAt: DateTime.now(),
+      ),
+      Message(
+        id: '4',
+        text: 'こんにちはこんにちはこんにちはこんにちはこんにちはこんにちは',
+        senderId: 'fuga',
+        createdAt: DateTime.now(),
+        updatedAt: DateTime.now(),
+      ),
+    ];
+    final String myUid = 'hoge';
+
+    return Scaffold(
+      appBar: AppBar(title: Text(userName)),
+      body: SafeArea(
+        child: ListView.builder(
+          reverse: true,
+          shrinkWrap: true,
+          physics: RangeMaintainingScrollPhysics(),
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          itemCount: messageList.length,
+          itemBuilder: (context, index) {
+            final message = messageList[index];
+            final isSendFromMe = message.senderId == myUid;
+            
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 12),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                spacing: 4,
+                textDirection: isSendFromMe ? TextDirection.rtl : TextDirection.ltr,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(8),
+                    constraints: BoxConstraints(
+                      maxWidth: MediaQuery.of(context).size.width * .6,
+                    ),
+                    decoration: BoxDecoration(
+                      color: isSendFromMe ? Colors.green : Colors.grey[300],
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Text(message.text),
+                  ),
+                  Text(DateFormat('HH:mm').format(message.createdAt)),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
