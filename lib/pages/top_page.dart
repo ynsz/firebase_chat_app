c0d10882d2a2b291705a45259ada4be6e4e078b0
ynsz <99311398+ynsz@users.noreply.github.com>
2026-02-05 20:37:10 +0900

    ファイル整理

---
+++ lib/pages/top_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class TopPage extends StatefulWidget {
+  const TopPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<TopPage> createState() => _TopPageState();
+}
+
+class _TopPageState extends State<TopPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
